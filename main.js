b3e0d7f3e4488c19a30c532a71f3f924b72bc130
Hayko Koryun <haykokoryun@gmail.com>
2015-10-22 01:29:11 +0400

    initial tcp server with test convert command

--- main.js
+++ main.js
@@ -9,21 +9,64 @@ config["import-path"] = config["import-path"].replace(/\\/ig, "\\\\");
 config["export-path"] = config["export-path"].replace(/\\/ig, "\\\\");
 
 var template = fs.readFileSync("template.rb", "utf8");
+var logo = fs.readFileSync("logo.txt", "utf8");
 
-fs.writeFileSync
-(
-  "export.rb",
-  template
-  .replace
+function convert()
+{
+  fs.writeFileSync
   (
-    "{{import}}",
-    config["import-path"] + "import.skp"
-  )
-  .replace
-  (
-    "{{export}}",
-    config["export-path"] + "export.kmz"
-  )
-);
+    "export.rb",
+    template
+    .replace
+    (
+      "{{import}}",
+      config["import-path"] + "import.skp"
+    )
+    .replace
+    (
+      "{{export}}",
+      config["export-path"] + "export.kmz"
+    )
+  );
 
-exec("\"" + config["sketchup-bin"] + "\" -RubyStartup " + __dirname + "\\export.rb");
+  exec("\"" + config["sketchup-bin"] + "\" -RubyStartup " + __dirname + "\\export.rb");
+}
+
+function commander(command, socket)
+{
+  switch(command)
+  {
+    case "convert":
+    {
+      socket.write("converting file\r\n");
+      convert();
+      socket.write("done\r\n");
+    } break;
+  }
+}
+
+var net = require('net');
+
+var server = net.createServer(function(socket)
+{
+  socket.setEncoding("utf8");
+	socket.write(logo + "\r\n");
+
+  var buffer = "";
+  socket.on('data', function(data)
+  {
+    if(data == "\r\n")
+    {
+      commander(buffer, socket);
+      buffer = "";
+    }
+    else
+    {
+      buffer += data;
+    }
+  });
+});
+
+server.listen(config.port, config.address);
+
+console.log(logo);
