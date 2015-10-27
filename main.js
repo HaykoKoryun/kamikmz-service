304b652f65529f6fd66532c1cbfb45a2eb8f374b
hayk.galstyan <hayk.galstyan@tumo.org>
2015-10-27 15:12:35 +0400

    it works

--- main.js
+++ main.js
@@ -101,11 +101,15 @@ var escort = (function()
     return connected;
   };
 
-  _instance.startReceive = function(size)
+  _instance.receive = function(size)
   {
     console.log("setting up file transfer, size: " + size);
 
-    fileStream = fs.createWriteStream(config["import-path"] + "import.skp", {defaultEncoding:"binary"});
+    fileStream = fs.createWriteStream
+    (
+      config["import-path"] + "import.skp",
+      {defaultEncoding:"binary"}
+    );
     socket.setEncoding("binary");
     socket.pipe(fileStream, {end:false});
     size = size;
@@ -129,6 +133,34 @@ var escort = (function()
       });
     }).then(function(result){return result;});
   }
+
+  _instance.send = function()
+  {
+    console.log("setting up file transfer");
+
+    fileStream = fs.createReadStream
+    (
+      config["export-path"] + "export.kmz",
+      {defaultEncoding:"binary"}
+    );
+
+    return new Promise(function(resolve, reject)
+    {
+      fileStream.on('open',function()
+      {
+        console.log("sending file");
+        fileStream.pipe(socket, {end:false});
+        socket.setEncoding("binary");
+        fileStream.on("end", function()
+        {
+          fileStream.unpipe(escort);
+          fileStream.close();
+          console.log("complete");
+          resolve("complete");
+        });
+      });
+    }).then(function(result){return result;});
+  };
 
   _instance.completed = function()
   {
@@ -185,7 +217,7 @@ var command = (function()
           return Promise.join
           (
             question("ready\n"),
-            escort.startReceive(answer)
+            escort.receive(answer)
           );
         }
         else
@@ -197,15 +229,16 @@ var command = (function()
         if(answers[0] == "complete" && answers[1] == "complete")
         {
           console.log("file received");
-          // 3. when conversion finished, tell client to get ready to receive
-          return question("exported\r\n");
+          convert();
+          return question("exported\n");
         }
       }).then(function(answer)
       {
         if(answer == "size")
         {
-          // 1. return the size of the file to send
-          // TODO return size
+          var stats = fs.statSync(config["export-path"] + "export.kmz");
+          var size = stats["size"];
+          return question(size + "\n");
         }
         else
         {
@@ -215,15 +248,19 @@ var command = (function()
       {
         if(answer == "ready")
         {
-          // 1. tell escort to start sending file
-          // 2. when escort finished sending file, return:
-          return question("complete");
+          console.log("ready to send exported file");
+
+          return Promise.join
+          (
+            escort.send(),
+            question("complete\n")
+          );
         }
-      }).then(function(answer)
+      }).then(function(answers)
       {
-        if(answer == "complete")
+        if(answers[0] == "complete" && answers[1] == "complete")
         {
-          // TODO task is complete, rinse, repeat!
+          console.log("conversion complete");
         }
       }).error(function(error)
       {
@@ -248,171 +285,6 @@ var command = (function()
   {
 
   }
-
-    /*var mode = MODE_WAITING;
-
-    var fileStream;
-
-    socket.on('data', function(data)
-    {
-      switch(mode)
-      {
-        case MODE_WAITING:
-        {
-          if(data.trim() != "convert") return;
-
-          console.log("convert command received, waiting for file");
-
-          socket.write("ok\n");
-          mode = MODE_RECEIVE;
-
-          fileStream = fs.createWriteStream(config["import-path"] + "import.skp", {defaultEncoding:"binary"});
-          socket.setEncoding("binary");
-          socket.pipe(fileStream, {end:false});
-        } break;
-
-        case MODE_RECEIVE:
-        {
-          console.log("receiving file");
-          mode = MODE_RECEIVING;
-        } break;
-
-        case MODE_RECEIVING:
-        {
-          if(data.trim() != "done") return;
-
-          socket.unpipe(fileStream);
-          fileStream.close();
-          console.log("done");
-
-          convert();
-
-          fileStream = fs.createReadStream(config["export-path"] + "export.kmz");
-
-          mode = MODE_SENDING;
-
-          fileStream.on("open", function()
-          {
-            console.log("sending exported file");
-            fileStream.pipe(socket, {end:false});
-
-            fileStream.on("end", function()
-            {
-              console.log("done");
-              fileStream.unpipe(socket);
-              socket.setNoDelay(true);
-              socket.write("\n\n\n", "utf8");
-              socket.write("done","utf8");
-              socket.setNoDelay(false);
-              mode = MODE_WAITING;
-              cleanTemp();
-            });
-          });
-
-        } break;
-
-        case MODE_SENDING:
-        {
-
-        } break;
-      }
-    });
-    */
 })();
 
 console.log(logo);
-
-/*
-var net = require('net');
-
-var server = net.createServer(function(conn){});
-
-server.listen(config.command, config.host, function(){});
-
-server.on("connection", function(socket)
-{
-  console.log("client connected");
-
-  socket.setEncoding("utf8");
-
-  var mode = MODE_WAITING;
-
-  var fileStream;
-
-  socket.on('data', function(data)
-  {
-    switch(mode)
-    {
-      case MODE_WAITING:
-      {
-        if(data.trim() != "convert") return;
-
-        console.log("convert command received, waiting for file");
-
-        socket.write("ok\n");
-        mode = MODE_RECEIVE;
-
-        fileStream = fs.createWriteStream(config["import-path"] + "import.skp", {defaultEncoding:"binary"});
-        socket.setEncoding("binary");
-        socket.pipe(fileStream, {end:false});
-      } break;
-
-      case MODE_RECEIVE:
-      {
-        console.log("receiving file");
-        mode = MODE_RECEIVING;
-      } break;
-
-      case MODE_RECEIVING:
-      {
-        if(data.trim() != "done") return;
-
-        socket.unpipe(fileStream);
-        fileStream.close();
-        console.log("done");
-
-        convert();
-
-        fileStream = fs.createReadStream(config["export-path"] + "export.kmz");
-
-        mode = MODE_SENDING;
-
-        fileStream.on("open", function()
-        {
-          console.log("sending exported file");
-          fileStream.pipe(socket, {end:false});
-
-          fileStream.on("end", function()
-          {
-            console.log("done");
-            fileStream.unpipe(socket);
-            socket.setNoDelay(true);
-            socket.write("\n\n\n", "utf8");
-            socket.write("done","utf8");
-            socket.setNoDelay(false);
-            mode = MODE_WAITING;
-            cleanTemp();
-          });
-        });
-
-      } break;
-
-      case MODE_SENDING:
-      {
-
-      } break;
-    }
-  });
-
-  socket.on("error", function()
-  {
-    socket.end();
-    cleanTemp();
-  });
-
-  socket.on("close", function()
-  {
-    cleanTemp();
-  });
-});
-*/
--- test.js
+++ test.js
@@ -11,6 +11,16 @@ var config = JSON.parse
 var command = new net.Socket();
 var escort = new net.Socket();
 
+command.on('error', function(err)
+{
+  console.log(err);
+});
+
+escort.on('error', function(err)
+{
+  console.log(err);
+});
+
 command.connect(config.command, "127.0.0.1", function()
 {
   console.log("command connected");
@@ -54,21 +64,76 @@ command.connect(config.command, "127.0.0.1", function()
           if(answer == "ready")
           {
             console.log("ready to send");
-            fileStream = fs.createReadStream("./test/original.skp");
+            fileStream = fs.createReadStream
+            (
+              "./test/original.skp",
+              {defaultEncoding:"binary"}
+            );
 
-            fileStream.on('open',function()
+            return new Promise(function(resolve, reject)
             {
-              console.log("sending file");
-              fileStream.pipe(escort, {end:false});
-              fileStream.on("end", function()
+              fileStream.on('open',function()
               {
-                console.log("complete");
-                fileStream.unpipe(escort);
-                fileStream.close();
-                return question("complete\n");
+                console.log("sending file");
+                fileStream.pipe(escort, {end:false});
+                fileStream.on("end", function()
+                {
+                  console.log("complete");
+                  fileStream.unpipe(escort);
+                  fileStream.close();
+                  resolve("complete");
+                });
               });
+            }).then(function(answer)
+            {
+              return question("complete\n");
             });
           }
+        }).then(function(answer)
+        {
+          if(answer == "exported")
+          {
+            return question("size\n");
+          }
+        }).then(function(answer)
+        {
+          console.log("setting up file transfer, size: " + answer);
+
+          fileStream = fs.createWriteStream
+          (
+            "./test/export.kmz",
+            {defaultEncoding:"binary"}
+          );
+          escort.setEncoding("binary");
+          escort.pipe(fileStream, {end:false});
+          var size = answer;
+
+          var received = 0;
+
+          return Promise.join
+          (
+            question("ready\n"),
+            new Promise(function(resolve, reject)
+            {
+              escort.on("data", function(data)
+              {
+                received += data.length;
+
+                if(received == size)
+                {
+                  escort.unpipe(fileStream);
+                  fileStream.close();
+                  completed = true;
+                  console.log("complete");
+                  resolve("complete");
+                }
+              });
+            }).then(function(result){return result;})
+          )
+        }).then(function(answer)
+        {
+          console.log("conversion complete");
+          return question("complete\n");
         });
       }
     });
@@ -79,76 +144,3 @@ escort.connect(config.escort, "127.0.0.1", function()
 {
   console.log("escort connected");
 });
-
-/*
-var MODE_SENDING = 0;
-var MODE_RECEIVING = 2;
-
-socket.connect(config.port, "127.0.0.1", function()
-{
-  console.log("connected to kamikmz-service server");
-
-  var mode = MODE_SENDING;
-
-  socket.setEncoding("utf8");
-
-  console.log("sending convert command");
-
-  var fileStream;
-
-  socket.on("data", function(data)
-  {
-    console.log(data);
-    console.log("###");
-    switch(mode)
-    {
-      case MODE_SENDING:
-      {
-        if(data.trim() != "ok") return;
-
-        console.log("received ok");
-
-        fileStream = fs.createReadStream("./test/original.skp");
-
-        fileStream.on('open',function()
-        {
-          console.log("sending file");
-          fileStream.pipe(socket, {end:false});
-          fileStream.on("end", function()
-          {
-            console.log("done");
-            fileStream.unpipe(socket);
-            fileStream.close();
-            socket.write("done","utf8");
-
-            fileStream = fs.createWriteStream("./test/exported.kmz", {defaultEncoding:"binary"});
-
-            socket.pipe(fileStream, {end:false});
-
-            console.log("receiving file");
-            mode = MODE_RECEIVING;
-          });
-        });
-
-      } break;
-
-      case MODE_RECEIVING:
-      {
-        if(data.trim() != "done") return;
-
-        socket.unpipe(fileStream);
-        fileStream.close();
-        console.log("done");
-        socket.end();
-      } break;
-    }
-  });
-
-  socket.write("convert\n", "utf8");
-});
-
-socket.on('error', function(err)
-{
-  console.log(err);
-});
-*/
